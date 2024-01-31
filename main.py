7803418b9b613250ced71109af92193277f891f3
Heizenberg <popov.roman91@gmail.com>
2024-01-31 21:19:39 +0200

    figured out the slider problem

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.core.window import Window
+from threading import Thread
 from kivy.graphics import Rectangle, Color
 from kivy.utils import get_color_from_hex
-from threading import Thread
 from sorting_algorithms.bubleSort import bubble_sort
 from sorting_algorithms.mergeSort import merge_sort
 from sorting_algorithms.quickSort import quick_sort
 from sorting_algorithms.selectionSort import selection_sort
 from sorting_algorithms.insertionSort import insertion_sort
+from kivy.clock import Clock
 from colors import *
 import random
 
@@ -25,6 +26,12 @@ algorithms = {
     "Insertion Sort": insertion_sort,
 }
 
+speed = {
+    "Slow": 0.1,
+    "Medium": 0.01,
+    "Fast": 0.001,
+}
+
 
 class SortVisualizer(BoxLayout):
     activeSorting = True
@@ -32,11 +39,17 @@ class SortVisualizer(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.visualizer = self.ids.visualize_canvas
-        self.display_rectangles()
+        self.rectangles = []
+        # self.display_rectangles()
+        self.generate(self.ids.slider, int(self.ids.slider.value))
+        Clock.schedule_once(self.bind_slider_value)
         self.bind(size=self.update)
 
+    def bind_slider_value(self, *args):
+        self.ids.slider.bind(value=self.generate)
+
     def display_rectangles(self):
-        self.generate(int(self.ids.slider.value), self.visualizer)
+        self.generate(int(self.ids.slider.value))
 
     def set_active(self):
         self.activeSorting = False
@@ -45,26 +58,36 @@ class SortVisualizer(BoxLayout):
         self.draw(self.rectangles, [RED for i in range(len(self.rectangles))])
 
     def set_speed(self):
-        if self.ids.speed.text == "Slow":
-            return 0.1
-        elif self.ids.speed.text == "Medium":
-            return 0.01
-        else:
-            return 0.001
+        return speed[self.ids.speed.text]
 
-    def generate(self, number, visualize):
-        self.rectangles = [random.randint(5, 400) for i in range(number)]
-        self.draw(self.rectangles, [RED for i in range(len(self.rectangles))])
+    def generate(self, instance, value):
+        self.rectangles = [random.randint(5, 400) for i in range(value)]
+        assert len(self.rectangles) == value
+        self.draw(self.rectangles, [RED] * len(self.rectangles))
 
     def sort(self):
         speed = self.set_speed()
         self.activeSorting = True
 
-        self.sorting = Thread(
-            target=algorithms[self.ids.algorithm.text],
-            args=(self.rectangles, self, speed),
-            daemon=True,
-        ).start()
+        # self.sorting = Thread(
+        #     target=algorithms[self.ids.algorithm.text],
+        #     args=(self.rectangles, self, speed),
+        #     daemon=True,
+        # ).start()
+        def sorting_algorithm():
+            # Run the sorting algorithm
+            algorithms[self.ids.algorithm.text](self.rectangles, self, speed)
+
+            # # Schedule a UI update on the main thread
+            # Clock.schedule_once(lambda dt: self.draw(self.rectangles, self.colorArray))
+
+        # Start the sorting algorithm on a separate thread
+        self.sorting = Thread(target=sorting_algorithm, daemon=True)
+        self.sorting.start()
+
+
+
+
 
     def draw(self, rects, colorArray):
         self.visualizer.canvas.clear()
@@ -84,29 +107,7 @@ class SortVisualizer(BoxLayout):
                 size_x = rect_width
                 Rectangle(pos=(pos_x, pos_y), size=(size_x, size_y))
         
-    # def calculate_dimensions(self, rects, width, height):
-    #     rect_width = width / (len(rects) + 1)
-    #     spacing = rect_width / 6
-    #     rect_width = rect_width - spacing
-    #     canvas_height = height - height / 10 - 20
-    #     normalized_rects = [i / max(rects) for i in rects]
-    #     return rect_width, spacing, canvas_height, normalized_rects
-
-    # def draw_rectangle(self, i, height, rect_width, spacing, canvas_height):
-    #     Color(*get_color_from_hex(colorArray[i]))
-    #     pos_y = 5
-    #     pos_x = i * (rect_width + spacing) + offset + spacing
-    #     size_y = height * canvas_height
-    #     size_x = rect_width
-    #     Rectangle(pos=(pos_x, pos_y), size=(size_x, size_y))
-
-    # def draw(self, rects, colorArray):
-    #     self.visualizer.canvas.clear()
-    #     width, height = Window.size
-    #     rect_width, spacing, canvas_height, normalized_rects = self.calculate_dimensions(rects, width, height)
-    #     with self.visualizer.canvas:
-    #         for i, height in enumerate(normalized_rects):
-    #             self.draw_rectangle(i, height, rect_width, spacing, canvas_height)
+    
 
     def update(self, *args):
         self.draw(self.rectangles, [RED for i in range(len(self.rectangles))])
